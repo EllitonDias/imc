808eada478c4e1e5e5887b41c3a1dd7b382d95d3
EllitonDias <92165656+EllitonDias@users.noreply.github.com>
2022-04-03 11:27:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,44 @@
-'use strict'
-
+"use strict";
 
 //variaveis
-let btn = document.getElementById('btn');
-let btnCalcular = document.getElementById('calcular');
-let btnLimpar = document.getElementById('limpar');
+let btn = document.getElementById("btn");
+let btnCalcular = document.getElementById("calcular");
+let btnLimpar = document.getElementById("limpar");
 
 //eventos
-btnCalcular.addEventListener('click', imc, false);
-btnLimpar.addEventListener('click', limpeza, false);
-btn.addEventListener('click',theme,false);
+btnCalcular.addEventListener("click", imc, false);
+btnLimpar.addEventListener("click", limpeza, false);
+btn.addEventListener("click", theme, false);
 
 //funções
 
-function theme(e){
-    document.body.classList.toggle('dark-theme')
-    var className = document.body.className;
-    if (className == 'light-theme') {
-        this.textContent = 'Pink';
-    } else {
-        this.textContent = 'light-theme';
-    }
-    console.log('Current class name:' +className);
+function theme(e) {
+  document.body.classList.toggle("dark-theme");
+  var className = document.body.className;
+  if (className == "light-theme") {
+    this.textContent = "Pink";
+  } else {
+    this.textContent = "light-theme";
+  }
+  console.log("Current class name:" + className);
 }
 
-
-
-
-
 function imc(e) {
-    let altura = document.getElementById('altura').value;
-    let peso = document.getElementById('peso').value;
+  let altura = document.getElementById("altura").value;
+  let peso = document.getElementById("peso").value;
 
-    if (altura === 0 || peso === 0) {
-        alert("por favor preencha todos os campos!");
-    }
-    else {
-        let alt = altura.replace(',', '.');
-        let pes = peso.replace(',', '.');
-        let imc = pes/ Math.pow(alt, 2).toFixed(1);
-        document.getElementById('res').innerHTML = imc;
-    }
+  if (altura == 0 || peso == 0) {
+    window.alert("por favor preencha todos os campos!");
+  } else {
+    let alt = altura.replace(",", ".");
+    let pes = peso.replace(",", ".");
+    let imc = pes / Math.pow(alt, 2).toFixed(1);
+    document.getElementById("res").innerHTML = imc;
+  }
 }
 
 function limpeza(e) {
-    document.getElementById('altura').value = "";
-    document.getElementById('peso').value = "";
-    document.getElementById('res').innerHTML = "";
+  document.getElementById("altura").value = "";
+  document.getElementById("peso").value = "";
+  document.getElementById("res").innerHTML = "";
 }
-
